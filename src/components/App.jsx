7187a5d1b5e1388e948d193b7ce4c2a132d948ab
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-10-06 00:49:37 +0300

    The code was refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,60 @@
-import React, { Component } from "react";
-
+import React, { useState } from "react";
 import { Section } from 'components/Section/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Statistics/Statistics';
 import { Notification } from 'components/Notification/Notification';
 
-
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   // Функція, що при натисканні на кнопку оновлює стейт екземпляра класу
-  onLeaveFeedback = event => {
-    this.setState( prevState => { 
-      return { [event.target.name]: (prevState[event.target.name] + 1) };
-    });
+  const onLeaveFeedback = event => {
+    switch(event.target.name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
   // Функція, що підраховує загальну кількість відгуків
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   }
   // Функція, що підраховує відсоток хороших відгуків відгуків
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
+  const countPositiveFeedbackPercentage = () => {
     return Math.round((good * 100) / (good+neutral+bad));
   } 
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const options = Object.keys(this.state);
+  const total = countTotalFeedback();
+  const options = [ good, neutral, bad ];
 
-    return (
-      <div className='app'>
-        <div className='app_container'>
-        <Section title="Please leave feedback">
-          <FeedbackOptions options={options} onLeaveFeedback={this.onLeaveFeedback}/>
-        </Section>
-        <Section title="Statistics">
-          {total ? 
-          <Statistics 
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-           /> :
-          <Notification message="There is no feedback"></Notification>
-          }
-          </Section>        
+  return (
+    <div className='app'>
+      <div className='app_container'>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback}/>
+      </Section>
+      <Section title="Statistics">
+        {total ? 
+        <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        positivePercentage={countPositiveFeedbackPercentage()}
+          /> :
+        <Notification message="There is no feedback"></Notification>
+        }
+        </Section>        
       </div>
-      </div>
-    );
-  };
-
+    </div>
+  );
 }
